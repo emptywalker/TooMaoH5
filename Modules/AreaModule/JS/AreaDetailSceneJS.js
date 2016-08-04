826ae695a4bbdd70c4bd496d4464d49e1f013872
xuyouhong <1647064983@qq.com>
2016-08-04 14:52:37 +0800

    add area detail scene
    complete area detail scene

--- Modules/AreaModule/JS/AreaDetailSceneJS.js
+++ Modules/AreaModule/JS/AreaDetailSceneJS.js
@@ -8,15 +8,24 @@ ready(function () {
     ajax({ url: path,
         success: function (responseText, response) {
             // 此处放成功后执行的代码
-            // console.log('responseText ===' + responseText);
-            // console.log('response ===' + response);
             var original = JSON.parse(responseText);
+            document.getElementById('top-image').src = original.regional.topImage;
+            document.getElementById('title').textContent = original.regional.title;
+            document.getElementById('description').textContent = original.regional.desc;
 
-            var topImage = document.getElementsByClassName('top-image').src = original.regional.topImage;
+            var productArray = new Array()
+            var html = '';
+            productArray = original.product;
+            for (var i = 0; i < productArray.length; i++){
+                let module =   productArray[i];
+                html += "<li> <a class='product' href='" + module.productUrl+ "'>  <img src=" + module.productImage +">  </a></li>";
+            }
+            document.getElementById("product-list").innerHTML = html;//"<a class='product' style='background-image:url("+ module.productImage +")'></a>";
 
         },
         fail: function (status) {
             // 此处放失败后执行的代码
+            console.log(status);
         }
     });
 
