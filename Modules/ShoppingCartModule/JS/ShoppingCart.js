d9c5f336229d4d6bf161f2fec2eaa9f06d8d00be
xuyouhong <1647064983@qq.com>
2016-08-04 17:28:11 +0800

    complete tabBar
    init all modules first page and tabBar

--- Modules/ShoppingCartModule/JS/ShoppingCart.js
+++ Modules/ShoppingCartModule/JS/ShoppingCart.js
@@ -1,24 +1,8 @@
 /**
- * Created by Administrator on 16/7/30.
+ * Created by Administrator on 16/8/4.
  */
-$(document).ready(function () {
-    //请求数据
-    var path = URLBase + '/1.2/regional/show';
-    getRequest('get', path, null, function completeOperation(data, textStatus) {
-        console.log(data);
-        $.each(data.results, function (i, item) {
-            $('ul').append(
-                "<li> <a style='background-image:url(" + item.cardImage +")' href = './AreaDetailScene.html?id=" + item.id + "' ></a></li>"
-            );
-        });
-        $('li').attr({'class':'areaContent'});
-        $("a").attr({'class':'aClass'});
 
-    }, function errorOperation(error) {
-        console.log(error);
-    });
-
-
+ready(function () {
     //tabBar点击处理
     document.getElementById('square').onclick = function () {
         // alert('square');
--- Util/JS/LifeCycleUtil.js
+++ Util/JS/LifeCycleUtil.js
@@ -17,3 +17,5 @@ function ready(fn){
         });
     }
 }
+
+
